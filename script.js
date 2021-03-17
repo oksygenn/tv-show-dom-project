260bf9b6e2412722a477b83c1d0226e9b93756d1
Oksana Kostenko <kseniavlad31@gmail.com>
2021-03-17 21:42:03 +0000

    completes level 200 and part of 300 js

--- script.js
+++ script.js
@@ -1,30 +1,80 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+let episodesContainer;
+let allEpisodes;
+let quantityOfEpisodes;
 
-function makePageForEpisodes(episodeList) {
+const setup = () => {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  episodesContainer = document.createElement("ul");
+  episodesContainer.className = "episodes-container";
+  quantityOfEpisodes = document.createElement("span");
 
+  rootElem.append(quantityOfEpisodes, episodesContainer);
+
+  // get and render all episodes to the page
+  allEpisodes = getAllEpisodes();
+  makePageForEpisodes(allEpisodes);
+  createSelectOptions(allEpisodes);
+
+  const searchInput = document.querySelector("#site-search");
+
+  searchInput.addEventListener("input", () => {
+    const value = searchInput.value;
+    if (value === "") {
+      makePageForEpisodes(allEpisodes);
+      return;
+    }
+
+    const filteredList = allEpisodes.filter((episode) => {
+      return (
+        episode.name.toLowerCase().includes(value.toLowerCase()) ||
+        episode.summary.toLowerCase().includes(value.toLowerCase())
+      );
+    });
+
+    makePageForEpisodes(filteredList);
+  });
+};
+
+const makePageForEpisodes = (episodeList) => {
+  episodesContainer.innerHTML = "";
+  quantityOfEpisodes.innerHTML = `Displaying ${episodeList.length} of ${allEpisodes.length}`;
   for (let episode of episodeList) {
-    const episodeContainer = document.createElement("div");
-    episodeContainer.className = "episode-container";
+    const episodeItem = document.createElement("li");
+    const episodeInfo = document.createElement("h2");
 
-    const header = document.createElement("h1");
-    header.innerText = `${episode.name} - S0${episode.season}E0${episode.number}`;
+    if (episode.number > 9) {
+      episodeInfo.innerText = `${episode.name} - S0${episode.season}E${episode.number}`;
+    } else {
+      episodeInfo.innerText = `${episode.name} - S0${episode.season}E0${episode.number}`;
+    }
 
-    const image = document.createElement("img");
-    image.src = episode.image.medium;
-
+    const episodeImage = document.createElement("img");
+    episodeImage.src = episode.image.medium;
     const episodeSummary = document.createElement("p");
-    // episodeSummary.className += "episode-description";
+    episodeSummary.className += "episode-summary";
     episodeSummary.innerHTML = `${episode.summary}`;
-    episodeContainer.append(header, episodeSummary, image);
-    rootElem.append(episodeContainer);
+
+    episodeItem.append(episodeInfo, episodeImage, episodeSummary);
+    episodesContainer.append(episodeItem);
   }
-}
+};
+
+// add <option> to <select>
+const createSelectOptions = (episodeList) => {
+  const selectEl = document.querySelector("#episodes");
+  for (let episode of episodeList) {
+    const option = document.createElement("option");
+    option.setAttribute("value", episode.name);
+
+    if (episode.number > 9) {
+      option.innerText = `S0${episode.season}E${episode.number} - ${episode.name}`;
+    } else {
+      option.innerText = `S0${episode.season}E0${episode.number} - ${episode.name}`;
+    }
+
+    selectEl.append(option);
+  }
+};
 
 window.onload = setup;
 
@@ -32,10 +82,12 @@ window.onload = setup;
 //   <div id="root">
 //     <label for="episode-search">Search the site:</label>
 //     <input type="search" id="episode-search" aria-label="Search through site content">
-//     <div class="episode-container">
-//       <h1>Name</h1>
-//       <img />
-//       <p class="episode-description"></p>
-//     </div>
+//     <ul class="episodes-container">
+//       <li class="episode">
+//         <h1>Name</h1>
+//         <img src="" alt="" />
+//         <p class="episode-description"></p>
+//       </li>
+//     </ul>
 //   </div>;
 // }
