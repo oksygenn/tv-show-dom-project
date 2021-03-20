0d69be669f395667597fa5b3779452918bfbbdb8
Oksana Kostenko <kseniavlad31@gmail.com>
2021-03-20 22:46:12 +0000

    partially styles the page

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let episodesContainer; // <ul> for all episodes
+// let showContainer;
+// let show;
+let episodesContainer; // <div> for all episodes (container)
+let listOfEpisodes; // ul
 let allEpisodes;
 let quantityOfEpisodes; // <span> to show how many episodes are shown on the page
 let selectEl; // <select> (to use in createSelectOptions and for event listener)
@@ -6,15 +9,17 @@ let selectEl; // <select> (to use in createSelectOptions and for event listener)
 const setup = () => {
   // create "containers" for episodes and append them to "root" element
   const rootElem = document.getElementById("root");
-  episodesContainer = document.createElement("ul");
-  episodesContainer.className = "episodes-container";
+  // showContainer = document.createElement("div");
+  episodesContainer = document.createElement("div");
+  episodesContainer.className += "episodes-container container";
   quantityOfEpisodes = document.createElement("span");
+  listOfEpisodes = document.createElement("div");
+  listOfEpisodes.className += "row row-cols-md-3 gx-5 list-of-episodes";
+  episodesContainer.append(listOfEpisodes);
   rootElem.append(quantityOfEpisodes, episodesContainer);
 
-  const showAllButton = document.querySelector("#show-all-episodes");
-  showAllButton.addEventListener("click", () =>
-    makePageForEpisodes(allEpisodes)
-  );
+  // show = getOneShow();
+  // renderShow(show);
 
   // get and render all episodes to the page
   allEpisodes = getAllEpisodes();
@@ -50,15 +55,24 @@ const setup = () => {
 
     makePageForEpisodes(filteredEpisodes);
   });
+
+  const showAllButton = document.querySelector("#show-all-episodes");
+  showAllButton.addEventListener("click", () =>
+    makePageForEpisodes(allEpisodes)
+  );
 };
 
 // function which takes a list of episodes, creates html tags for each episode and renders those episodes on the page.
 const makePageForEpisodes = (episodeList) => {
-  episodesContainer.innerHTML = "";
+  listOfEpisodes.innerHTML = "";
   quantityOfEpisodes.innerHTML = `Displaying ${episodeList.length} of ${allEpisodes.length}`;
   for (let episode of episodeList) {
-    const episodeItem = document.createElement("li");
-    const episodeInfo = document.createElement("h2");
+    const episodeItem = document.createElement("div");
+    episodeItem.className += "col item";
+    const episodeContainer = document.createElement("div");
+    episodeContainer.className += "p-3 h-100 inside";
+    const episodeInfo = document.createElement("h4");
+    episodeInfo.className += "episode-title";
 
     if (episode.number > 9) {
       episodeInfo.innerText = `${episode.name} - S0${episode.season}E${episode.number}`;
@@ -67,19 +81,30 @@ const makePageForEpisodes = (episodeList) => {
     }
 
     const episodeImage = document.createElement("img");
+    episodeImage.className += "episode-image";
     episodeImage.src = episode.image.medium;
-    const episodeSummary = document.createElement("p");
+    const episodeSummary = document.createElement("div");
     episodeSummary.className += "episode-summary";
     episodeSummary.innerHTML = `${episode.summary}`;
 
-    episodeItem.append(episodeInfo, episodeImage, episodeSummary);
-    episodesContainer.append(episodeItem);
+    episodeItem.append(episodeContainer);
+    episodeContainer.append(episodeInfo, episodeImage, episodeSummary);
+    listOfEpisodes.append(episodeItem);
   }
 };
 
+// const renderShow = () => {
+//   const nameOfTheShow = document.createElement("h1");
+//   const showImage = document.createElement("img");
+//   nameOfTheShow.innerText = show.name;
+//   showImage.src = show.image.medium;
+
+//   showContainer.append(nameOfTheShow, showImage);
+// };
+
 // creates and adds options to <select> (selectEl) tag
 const createSelectOptions = (episodeList) => {
-  selectEl = document.querySelector("#episodes");
+  selectEl = document.querySelector("#select-episodes");
 
   for (let episode of episodeList) {
     const option = document.createElement("option");
@@ -95,3 +120,14 @@ const createSelectOptions = (episodeList) => {
 };
 
 window.onload = setup;
+{
+  /* <div container>
+  <ul row>
+    <li col>
+      <h4></h4>
+      <img />
+      <p></p>
+    </li>
+  </ul>
+</div>; */
+}
